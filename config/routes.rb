deb98fc1534b23060302242ad7c80201ae5db042
Elena Ceceva <elena.ceceva@webfactory.mk>
2020-02-20 18:14:57 +0100

    Basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get '/signup',  to: 'users#new'
+  get '/home', to: 'static_pages#home'
+  root 'static_pages#home'
+  resources :users
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get home_path
+    assert_response :success
+  end
+
+  test "should get root" do
+    get home_path
+    assert_response :success
+  end
+
+  test "should get new" do
+    get signup_path
+    assert_response :success
+  end
+end
