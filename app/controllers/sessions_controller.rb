a33bf669b665ceb844a54c9694805130a9d5ec8f
Elena Ceceva <elena.ceceva@webfactory.mk>
2020-02-21 11:56:57 +0100

    Sign up and log in done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      else
-    render 'new'
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      redirect_to '/login'
     end
   end
-  def destroy
-  end
+    def destroy
+      log_out
+      redirect_to root_url
+    end
 end
