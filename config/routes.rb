ffac0b545cb8e9c772c328969cc6de2529daa356
Elena Ceceva <elena.ceceva@webfactory.mk>
2020-02-24 17:28:09 +0100

    added logout

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :users
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 
   resources :to_dos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
